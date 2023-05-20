ddcdb3a377c4d9813d32a8508b20e37feaeac236
PierreAboussouan <112406629+PierreAboussouan@users.noreply.github.com>
2023-05-20 19:54:38 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    for index in range(101):
+        basic.show_number(index)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function on_forever() {
+    for (let index = 0; index < 101; index++) {
+        basic.showNumber(index)
+    }
+})
